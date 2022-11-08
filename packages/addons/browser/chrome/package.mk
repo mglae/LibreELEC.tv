361dcbbc928aad7e213737fb37aa5719c2aec4fd
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-08 10:04:24 +0000

    chrome: update to 107.0.5304.87 and addon (114)

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
 # curl -s http://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages | grep -B 1 Version
-PKG_VERSION_NUMBER="103.0.5060.134"
+PKG_VERSION_NUMBER="107.0.5304.87"
 PKG_REV="113"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
